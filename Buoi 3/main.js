45c2480007f3c73a882d85a82e1488f366e850f0
Nguyen Dang Phuc <phucdangzai@gmail.com>
2026-06-09 20:58:50 +0700

    gg

--- Buoi 3/main.js
+++ Buoi 3/main.js
@@ -1,13 +1,16 @@
-let button = document.getElementById("btnbutton");
+let addbutton = document.getElementById("addbutton");
+let corebuttons = document.getElementById("corebutton");
+let otherbuttons = document.getElementById("otherbutton");
 let input = document.getElementById("btninput");
-let skill = document.querySelector(".skill .tags");
-const div = document.querySelector("div:nth-child(2)");
-const div = document.querySelector("p:nth-of-type(1)");
-button.addEventListener("click", function () {
-  skill.innerHTML += `<span> ${input.value} </span>`;
+let coreskill = document.querySelector(".skill .tags");
+let otherskill = document.querySelector(".other .tags");
+// const div = document.querySelector("div:nth-child(2)");
+// const div = document.querySelector("p:nth-of-type(1)");
+corebuttons.addEventListener("click", function () {
+  if (input.value === "") return;
+  coreskill.innerHTML += `<span> ${input.value} </span>`;
 });
-const divs=document.querySelectorAll("div");
-divs.forEach(
-  function(div)
-)
-div.addEventListener("click" )
+otherbuttons.addEventListener("click", function () {
+  if (input.value === "") return;
+  otherskill.innerHTML += `<span> ${input.value} </span>`;
+});
