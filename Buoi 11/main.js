e139c42bcf6efccd5aef60a0c87b1c0a8b47d56d
Nguyen Dang Phuc <phucdangzai@gmail.com>
2026-06-11 19:16:48 +0700

    jj

--- Buoi 11/main.js
+++ Buoi 11/main.js
@@ -4,39 +4,3 @@ let menu = document.querySelector(".menu-item  ");
 div.addEventListener("click", function () {
   menu.classList.toggle("show");
 });
-const data=fetch("");
-console.log(data)
-const promise =new Promise((resolve,reject)
-)
-setTimeout(()=>{
-  resolve("da hoan thnah")},1000)
-})
-promise
-.then ((data)=>{
-console.log(data)
-})
-.catch((error)=>{console.log("error")}
-
-))
-login ()
-.then ((user)=>{
-  console.log("get user")
-})
-.then ((post)=>{
-  console.LOG ("GET POST")
-})
-.catch((error)=>{
-  console.log("error")
-})
-async function login() {
-  try {
-    const user=await login();
-    console.log("get user")
-    const post =await getComputedStyle(user.id)
-    console.log("get post")
-  }
-  catch(error){
-    console.log(error);
-  }
-  
-}
