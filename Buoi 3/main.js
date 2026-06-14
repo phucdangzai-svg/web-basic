71f026ae6da1ec09112b9f3c08c604ef56e0e78a
Nguyen Dang Phuc <phucdangzai@gmail.com>
2026-06-14 17:58:51 +0700

    finish  btvn lesson 11

--- Buoi 3/main.js
+++ Buoi 3/main.js
@@ -1,16 +1,16 @@
-let addbutton = document.getElementById("addbutton");
-let corebuttons = document.getElementById("corebutton");
-let otherbuttons = document.getElementById("otherbutton");
-let input = document.getElementById("btninput");
-let coreskill = document.querySelector(".skill .tags");
-let otherskill = document.querySelector(".other .tags");
-// const div = document.querySelector("div:nth-child(2)");
-// const div = document.querySelector("p:nth-of-type(1)");
-corebuttons.addEventListener("click", function () {
-  if (input.value === "") return;
-  coreskill.innerHTML += `<span> ${input.value} </span>`;
-});
-otherbuttons.addEventListener("click", function () {
-  if (input.value === "") return;
-  otherskill.innerHTML += `<span> ${input.value} </span>`;
-});
+// let addbutton = document.getElementById("addbutton");
+// let corebuttons = document.getElementById("corebutton");
+// let otherbuttons = document.getElementById("otherbutton");
+// let input = document.getElementById("btninput");
+// let coreskill = document.querySelector(".skill .tags");
+// let otherskill = document.querySelector(".other .tags");
+// // const div = document.querySelector("div:nth-child(2)");
+// // const div = document.querySelector("p:nth-of-type(1)");
+// corebuttons.addEventListener("click", function () {
+//   if (input.value === "") return;
+//   coreskill.innerHTML += `<span> ${input.value} </span>`;
+// });
+// otherbuttons.addEventListener("click", function () {
+//   if (input.value === "") return;
+//   otherskill.innerHTML += `<span> ${input.value} </span>`;
+// });
